7cc00855eb4a82e1a30e2c4f5940d17f4ae71643
Murchi <murchana.23@gmail.com>
2020-01-24 00:10:18 +0530

    Bubble Sort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+
+public class InsertionSort {
+
+	public static void insertionSort(int arr[]) {
+
+		int n = arr.length;
+		for(int i =1;i<n;i++)	 {
+
+			int k = arr[i];
+			int j = i-1;
+			while(j>=0 && arr[j]>k){
+
+				arr[j+1]=arr[j];
+				j=j-1;
+			}
+			arr[j+1]= k;
+		}	
+
+	}
+
+	public static void main(String args[]) {
+		
+		int arr[] = new int[] {2,5,6,1,10,16};
+		insertionSort(arr);
+		for(int i=0;i<arr.length;i++) {
+			
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
